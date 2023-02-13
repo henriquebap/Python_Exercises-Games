b9266afd616b7fff78d9a69334dd7884cc2beff8
Henriquebap <henriquebaptista2003@gmail.com>
2023-02-13 19:41:05 -0300

    forca.py - List comprehension

--- forca.py
+++ forca.py
@@ -3,8 +3,8 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana".upper()
-    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    palavra_secreta = "henrqiue".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     acertou = False
     enforcou = False
@@ -25,6 +25,8 @@ def jogar():
                 index += 1
         else:
             erros += 1
+
+            print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
 
         enforcou = erros  == 6
         acertou = "_" not in letras_acertadas
