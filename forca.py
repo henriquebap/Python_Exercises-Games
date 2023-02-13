15c45551fd880b2eb166a3d89ae8d43ce5cfac8a
Henriquebap <henriquebaptista2003@gmail.com>
2023-02-13 19:27:14 -0300

    forca.py - Stipulating error attempts at the game.

--- forca.py
+++ forca.py
@@ -3,27 +3,37 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ['_', '_', '_', '_', '_', '_']
 
     acertou = False
     enforcou = False
+    erros = 0
 
     print (letras_acertadas)
 
     while(not acertou and not enforcou):
 
-            chute = input ("Qual a Letra? -- ")
-            chute = chute.strip()
+        chute = input ("Qual a Letra? -- ")
+        chute = chute.strip().upper()
 
+        if (chute in palavra_secreta):
             index = 0
             for letra in palavra_secreta:
-                if(chute.upper() == letra.upper() ):
+                if(chute == letra):
                     letras_acertadas [index] = letra
-                index = index + 1
+                index += 1
+        else:
+            erros += 1
 
-            print(letras_acertadas)
+        enforcou = erros  == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
 
+    if (acertou):
+        print("Boa! Voce acertou a palavra ;) ")
+    else:
+        print("Putz, tu errou tudo hein, acabaram as suas tentativas")
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
